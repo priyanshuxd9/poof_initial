88405f37398555b55904d29daffd498303f85c72
Priyanshu Darshan <darshanpriyanshu.007@gmail.com>
2025-06-23 18:34:07 +0000

    make this panel stay on the dashboard only too i dont want this anywhere

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -55,7 +55,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className="flex min-h-screen flex-col bg-background">
-      <AppHeader />
+      {isDashboardPage && <AppHeader />}
       <main className="flex-1 flex flex-col overflow-hidden">
         {children}
       </main>
