19666634a1e8ff9315be52b4fd8a5d4e008fcb8d
Priyanshu Darshan <darshanpriyanshu.007@gmail.com>
2025-06-25 19:31:02 +0000

    reduce glow's feather intensity

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -72,7 +72,7 @@ const config: Config = {
   			sm: 'calc(var(--radius) - 4px)'
   		},
       boxShadow: {
-        'glow-primary': '0px 5px 15px rgba(0, 0, 0, 0.05), 0px 0px 40px hsl(var(--primary) / 0.3)',
+        'glow-primary': '0px 5px 15px rgba(0, 0, 0, 0.05), 0px 0px 25px hsl(var(--primary) / 0.3)',
       },
   		keyframes: {
   			'accordion-down': {
